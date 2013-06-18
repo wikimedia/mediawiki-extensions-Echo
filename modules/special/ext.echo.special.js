ab592dad6f2283d624fd5e20fddad015e56cad52
Krenair <krenair@gmail.com>
2013-06-18 16:23:35 +0100

    Cleanup JS
    * Add missing spaces after 'function'
    * Remove underscores from identifiers
    * Don't use typeof for undefined with local variables and object properties
    * API ok/err is deprecated since MediaWiki 1.20 (< 1.22-alpha), use .done and .fail
    
    Change-Id: I5ca3403ee263a0c3e6709618e48e896340952337

--- modules/special/ext.echo.special.js
+++ modules/special/ext.echo.special.js
@@ -1,4 +1,4 @@
-( function( $, mw ) {
+( function ( $, mw ) {
 	'use strict';
 
 	mw.echo.special = {
@@ -10,7 +10,7 @@
 		/**
 		 * Initialize the property in special notification page.
 		 */
-		initialize: function() {
+		initialize: function () {
 			var skin = mw.config.get('skin');
 
 			// Convert more link into a button
@@ -19,7 +19,7 @@
 				// Override jQuery UI button margins
 				.css( 'margin', '0.5em 0 0 0' )
 				.addClass( 'ui-button-blue' )
-				.click( function( e ) {
+				.click( function ( e ) {
 					e.preventDefault();
 					if ( !mw.echo.special.processing ) {
 						mw.echo.special.processing = true;
@@ -32,17 +32,17 @@
 
 			// Set up each individual notification with eventlogging, a close
 			// box and dismiss interface if it is dismissable.
-			$( '.mw-echo-notification' ).each( function() {
+			$( '.mw-echo-notification' ).each( function () {
 				mw.echo.setupNotificationLogging( $( this ), 'archive' );
 				if ( $( this ).find( '.mw-echo-dismiss' ).length ) {
 					mw.echo.setUpDismissability( this );
 				}
 			} );
 
-			$( '#mw-echo-moreinfo-link' ).click( function() {
+			$( '#mw-echo-moreinfo-link' ).click( function () {
 				mw.echo.logInteraction( 'ui-help-click', 'archive' );
 			} );
-			$( '#mw-echo-pref-link' ).click( function() {
+			$( '#mw-echo-pref-link' ).click( function () {
 				mw.echo.logInteraction( 'ui-prefs-click', 'archive' );
 			} );
 
@@ -64,105 +64,96 @@
 		/**
 		 * Load more notification records.
 		 */
-		loadMore: function() {
-			var api = new mw.Api(), notifications, data, container, $li, _this = this, unread = [];
+		loadMore: function () {
+			var api = new mw.Api(), notifications, data, container, $li, that = this, unread = [];
 
-			api.get(
-				{
-					'action' : 'query',
-					'meta' : 'notifications',
-					'notformat' : 'html',
-					'notprop' : 'index|list',
-					'notcontinue': this.notcontinue,
-					'notlimit': mw.config.get( 'wgEchoDisplayNum' )
-				},
-				{
-					'ok' : function( result ) {
-						container = $( '#mw-echo-special-container' );
-						notifications = result.query.notifications;
-						unread = [];
+			api.get( {
+				'action' : 'query',
+				'meta' : 'notifications',
+				'notformat' : 'html',
+				'notprop' : 'index|list',
+				'notcontinue': this.notcontinue,
+				'notlimit': mw.config.get( 'wgEchoDisplayNum' )
+			} ).done( function ( result ) {
+				container = $( '#mw-echo-special-container' );
+				notifications = result.query.notifications;
+				unread = [];
 
-						$.each( notifications.index, function( index, id ) {
-							data = notifications.list[id];
+				$.each( notifications.index, function ( index, id ) {
+					data = notifications.list[id];
 
-							if ( _this.header !== data.timestamp.date ) {
-								_this.header = data.timestamp.date;
-								$( '<li></li>' ).addClass( 'mw-echo-date-section' ).append( _this.header ).appendTo( container );
-							}
-
-							$li = $( '<li></li>' )
-								.data( 'details', data )
-								.data( 'id', id )
-								.addClass( 'mw-echo-notification' )
-								.attr( {
-									'data-notification-category': data.category,
-									'data-notification-event': data.id,
-									'data-notification-type': data.type
-								} )
-								.append( data['*'] )
-								.appendTo( container );
-
-							if ( !data.read ) {
-								$li.addClass( 'mw-echo-unread' );
-								unread.push( id );
-							}
-
-							mw.echo.setupNotificationLogging( $li, 'archive' );
-
-							if ( $li.find( '.mw-echo-dismiss' ).length ) {
-								mw.echo.setUpDismissability( $li );
-							}
-						} );
-
-						_this.notcontinue = notifications['continue'];
-						if ( unread.length > 0 ) {
-							_this.markAsRead( unread );
-						} else {
-							_this.onSuccess();
-						}
-					},
-					'err' : function() {
-						_this.onError();
+					if ( that.header !== data.timestamp.date ) {
+						that.header = data.timestamp.date;
+						$( '<li></li>' ).addClass( 'mw-echo-date-section' ).append( that.header ).appendTo( container );
 					}
+
+					$li = $( '<li></li>' )
+						.data( 'details', data )
+						.data( 'id', id )
+						.addClass( 'mw-echo-notification' )
+						.attr( {
+							'data-notification-category': data.category,
+							'data-notification-event': data.id,
+							'data-notification-type': data.type
+						} )
+						.append( data['*'] )
+						.appendTo( container );
+
+					if ( !data.read ) {
+						$li.addClass( 'mw-echo-unread' );
+						unread.push( id );
+					}
+
+					mw.echo.setupNotificationLogging( $li, 'archive' );
+
+					if ( $li.find( '.mw-echo-dismiss' ).length ) {
+						mw.echo.setUpDismissability( $li );
+					}
+				} );
+
+				that.notcontinue = notifications['continue'];
+				if ( unread.length > 0 ) {
+					that.markAsRead( unread );
+				} else {
+					that.onSuccess();
 				}
-			);
+			} ).fail( function () {
+				that.onError();
+			} );
 		},
 
 		/**
 		 * Mark notifications as read.
 		 */
-		markAsRead: function( unread ) {
-			var api = new mw.Api(), _this = this;
+		markAsRead: function ( unread ) {
+			var api = new mw.Api(), that = this;
 
 			api.get( {
 				'action' : 'query',
 				'meta' : 'notifications',
 				'notmarkread' : unread.join( '|' ),
 				'notprop' : 'count'
-			}, {
-				'ok' : function( result ) {
-					// update the badge if the link is enabled
-					if ( typeof result.query.notifications.count !== 'undefined' &&
-						$( '#pt-notifications').length && typeof mw.echo.overlay === 'object'
-					) {
-						mw.echo.overlay.updateCount( result.query.notifications.count );
-					}
-					_this.onSuccess();
-				},
-				'err': function() {
-					_this.onError();
+			} ).done( function ( result ) {
+				// update the badge if the link is enabled
+				if ( result.query.notifications.count !== undefined &&
+					$( '#pt-notifications').length && typeof mw.echo.overlay === 'object'
+				) {
+					mw.echo.overlay.updateCount( result.query.notifications.count );
 				}
+				that.onSuccess();
+			} ).fail( function () {
+				that.onError();
 			} );
 		},
 
-		onSuccess: function() {
+		onSuccess: function () {
 			if ( !this.notcontinue ) {
 				$( '#mw-echo-more' ).hide();
 			}
 			this.processing = false;
 		},
 
-		onError: function() {
+		onError: function () {
 			// Todo: Show detail error message based on error code
 			$( '#mw-echo-more' ).text( mw.msg( 'echo-load-more-error' ) );
 			this.processing = false;
