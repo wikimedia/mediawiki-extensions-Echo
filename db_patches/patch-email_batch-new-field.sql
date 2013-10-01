85181d3aa14613e233feb267b1f8741d32593679
Dereckson <dereckson@espace-win.org>
2013-10-02 00:56:56 +0200

    SQLite compatibility for Echo database patches
    These patches allow Echo update to stay compatible with SQLite.
    
    Change-Id: Ie4d0b4ec40bcc504096e87f469be0e96271daec1

--- db_patches/patch-email_batch-new-field.sql
+++ db_patches/patch-email_batch-new-field.sql
@@ -1,4 +1,4 @@
-ALTER TABLE /*_*/echo_email_batch ADD COLUMN eeb_event_hash varchar(32) binary not null;
+ALTER TABLE /*_*/echo_email_batch ADD COLUMN eeb_event_hash varchar(32) binary not null default '';
 
 DROP INDEX /*i*/echo_email_batch_user_priority_event ON /*_*/echo_email_batch;
 
