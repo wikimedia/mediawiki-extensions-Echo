50790b2965aa6175232b6a5362c46b2543d699d4
Cmcmahon <cmcmahon@wikimedia.org>
2014-09-17 16:26:27 -0700

    QA: refactor for consistent Ruby practice
    * use symbols not strings for element labels
    * use double quotes except where single quotes required
    
    Change-Id: I4eef50206c526611da535e529035c076d5bce266

--- tests/browser/features/support/pages/article_page.rb
+++ tests/browser/features/support/pages/article_page.rb
@@ -2,12 +2,12 @@ class ArticlePage
   include PageObject
   include URL
   page_url URL.url("<%=params[:article_name]%><%=params[:hash]%>")
-  li("flyout_link_container", css: "#pt-notifications")
-  a("flyout_link", css: "#pt-notifications a")
-  div("flyout", css: ".mw-echo-overlay")
+  li(:flyout_link_container, css: "#pt-notifications")
+  a(:flyout_link, css: "#pt-notifications a")
+  div(:flyout, css: ".mw-echo-overlay")
 
   # Overlay header
-  a("alert_tab_link", css: '.mw-echo-overlay-title ul li a', index: 1)
-  button("mark_as_read", css: '.mw-echo-notifications > button')
+  a(:alert_tab_link, css: ".mw-echo-overlay-title ul li a", index: 1)
+  button(:mark_as_read, css: ".mw-echo-notifications > button")
   a(:messages_view_link, css: ".mw-ui-active")
 end
