4961e32b53e0c27aeda12be56cc6050a75b2d334
Cmcmahon <cmcmahon@wikimedia.org>
2014-09-23 14:12:27 -0700

    QA: add modern assertions to flyout tests
    "Then" steps should always contains RSpec assertions. In the
    past this meant using some version of the word "should", but
    the modern convention in RSpec is to use "expect()" as I
    did here.
    
    Change-Id: I1c438a310803e29c0985328a76d2ec08578e0cb0

--- tests/browser/features/step_definitions/flyout_steps.rb
+++ tests/browser/features/step_definitions/flyout_steps.rb
@@ -3,13 +3,13 @@ When(/^I click the notification flyout button$/) do
 end
 
 Then(/^I do not see the notification flyout button$/) do
-  on(ArticlePage).flyout_link_container_element.when_not_present
+  expect(on(ArticlePage).flyout_link_container_element).not_to be_visible
 end
 
 Then(/^I see the notification flyout$/) do
-  on(ArticlePage).flyout_element.when_present
+  expect(on(ArticlePage).flyout_element.when_present).to be_visible
 end
 
 Then(/^I see the notification flyout button$/) do
-  on(ArticlePage).flyout_link_container_element.when_present
+  expect(on(ArticlePage).flyout_link_container_element.when_present).to be_visible
 end
