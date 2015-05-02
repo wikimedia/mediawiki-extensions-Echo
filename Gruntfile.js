5aefeb3f2e9e8c609304cdfe66a7c681ad48399d
Roan Kattouw <roan.kattouw@gmail.com>
2015-05-01 20:31:16 -0700

    Add Grunt infrastructure for CI
    * Update package.json based on Flow's
    * Add Gruntfile.js based on Flow's
    * Add .csslintrc and .jscsrc with rules to silence errors for now
    
    Change-Id: Ia8e23537d8dccbcff9ee336f667dc463590c0235

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+/*!
+ * Grunt file
+ *
+ * @package Flow
+ */
+
+/*jshint node:true */
+module.exports = function ( grunt ) {
+	grunt.loadNpmTasks( 'grunt-contrib-csslint' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-jscs' );
+
+	grunt.initConfig( {
+		jshint: {
+			options: {
+				jshintrc: true
+			},
+			all: [
+				'*.js',
+				'modules/**/*.js',
+				'tests/qunit/**/*.js'
+			]
+		},
+		jscs: {
+			src: '<%= jshint.all %>'
+		},
+		csslint: {
+			options: {
+				csslintrc: '.csslintrc'
+			},
+			all: 'modules/**/*.css'
+		},
+		banana: {
+			all: 'i18n/'
+		},
+		watch: {
+			files: [
+				'.{csslintrc,jscsrc,jshintignore,jshintrc}',
+				'<%= jshint.all %>',
+				'<%= csslint.all %>'
+			],
+			tasks: 'test'
+		}
+	} );
+
+	grunt.registerTask( 'lint', [ 'jscs', 'jshint', 'csslint', 'banana' ] );
+	grunt.registerTask( 'test', 'lint' );
+	grunt.registerTask( 'default', 'test' );
+};
