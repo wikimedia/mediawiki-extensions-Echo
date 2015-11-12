10c4ed6f49af1eafd8a56acc4298f6eb07eb4a37
Stephane Bisson <sbisson@wikimedia.org>
2015-11-12 12:06:11 -0500

    Browser Tests: fix
    * extract notifications to components/notifications.rb
    * wait for the flyout to be loaded before counting
      the unread notifications
    * remove popup.feature because it is redudant with
      notifications.feature and too low-level
      for acceptance testing
    
    Change-Id: If0b0286e8e98e379ae1d6d91db8084adda93b3f6

---
+++ tests/browser/features/support/components/notifications.rb
@@ -0,0 +1,19 @@
+class Notifications
+  include PageObject
+
+  link(:badge)
+  link(:badge_unseen, css: '.mw-echo-unseen-notifications')
+  link(:mark_all_as_read, css: '.mw-echo-ui-notificationsWidget-markAllReadButton > a')
+  div(:popup, css: '.mw-echo-ui-notificationBadgeButtonPopupWidget-popup')
+  span(:title, css: '.oo-ui-popupWidget-head > .oo-ui-labelElement-label')
+  div(:notifications_container, css: '.mw-echo-ui-notificationsWidget')
+
+  def when_loaded
+    title_element.when_present
+    notifications_container_element.when_present
+  end
+
+  def num_unread_notifications
+    div_elements(css: '.mw-echo-ui-notificationOptionWidget-unread').size
+  end
+end
