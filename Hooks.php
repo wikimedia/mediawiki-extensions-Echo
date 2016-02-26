23ebee2a619e99dd699255b50e7c9ca140b81d8c
James D. Forrester <jforrester@wikimedia.org>
2016-02-26 15:51:25 -0800

    Provide three more edit threshold congratulatory notifications
    Change-Id: Icd213fe7dd7ab8451432d35d6ee289c49449d5d1

--- Hooks.php
+++ Hooks.php
@@ -454,7 +454,7 @@ class EchoHooks {
 
 		// If the user is not an IP and this is not a null edit,
 		// test for them reaching a congratulatory threshold
-		$thresholds = array( 1, 10, 100, 1000 );
+		$thresholds = [ 1, 10, 100, 1000, 10000, 100000, 1000000 ];
 		if ( $user->isLoggedIn() && $status->value['revision'] ) {
 			// This edit hasn't been added to the edit count yet
 			$editCount = $user->getEditCount() + 1;
