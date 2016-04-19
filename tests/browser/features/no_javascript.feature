f9d2aacd6760cc64358640cd55b65ae50cbe5923
Stephane Bisson <sbisson@wikimedia.org>
2016-04-19 10:32:55 -0400

    Browser tests: fix nojs tests
    Since 79e095fd8a609b71937057034ff5801c13180137,
    modern js experience is mostly controlled by feature
    detection instead of user agent sniffing.
    
    This broke the Echo nojs browser tests and the
    mwext-mw-selenium job is now failing on ALL Echo patches.
    
    This change introduces a user agent string that is
    both still considered a nojs browser by RL and a
    desktop browser by MF.
    
    Change-Id: I34e8d9f0d879b404d95e674814744562fd57b397

--- tests/browser/features/no_javascript.feature
+++ tests/browser/features/no_javascript.feature
@@ -2,7 +2,7 @@
 Feature: Basic features for no-js functionality
 
   Background:
-    Given I am using user agent "Mozilla/4.0 (compatible; MSIE 5.5b1; Mac_PowerPC)"
+    Given I am using a nojs browser
 
   Scenario: Clicking alerts badge goes to Special:Notifications
     Given I am logged in
--- tests/browser/features/step_definition/no_javascript.rb
+++ tests/browser/features/step_definition/no_javascript.rb
@@ -3,8 +3,11 @@
 # Therefore it should run without any "when_present" clauses
 # If you need a "when_present" to make the test run, that is a bug
 
-Given(/^I am using user agent "(.+)"$/) do |user_agent|
-  browser_factory.override(browser_user_agent: user_agent)
+Given(/^I am using a nojs browser$/) do
+  # The following user-agent string contains:
+  #   SymbianOS: for RL to NOT load the modern experience
+  #   SMART-TV-SamsungBrowser: to bypass mobile-frontend and stay on the desktop site
+  browser_factory.override(browser_user_agent: 'SymbianOS,SMART-TV-SamsungBrowser')
 end
 
 Given(/^I am on Special Notifications page$/) do
