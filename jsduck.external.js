12eff85cf7b7b4da236741237737debec9591429
Timo Tijhof <krinklemail@gmail.com>
2016-09-21 00:51:03 +0100

    jsduck: Remove custom @source tag
    Only used in external.js to render a simple link. These methods
    don't have a regular description and as such the link renders
    effectively the same way in the same place without `@source`.
    
    Change-Id: I83cd524b6b53e67c5c2d18258c804ebd49a29b74

--- jsduck.external.js
+++ jsduck.external.js
@@ -1,47 +1,48 @@
 /**
+ * Source: <https://api.jquery.com/>
  * @class jQuery
- * <http://api.jquery.com/>
  */
 
 /**
+ * Source: <https://api.jquery.com/jQuery.ajax/>
  * @method ajax
- * @source <http://api.jquery.com/jQuery.ajax/>
+ * @static
  * @return {jqXHR}
  */
 
 /**
+ * Source: <https://api.jquery.com/Types/#Event>
  * @class jQuery.Event
- * @source <http://api.jquery.com/Types/#Event>
  */
 
 /**
+ * Source: <https://api.jquery.com/Types/#Promise>
  * @class jQuery.Promise
- * @source <http://api.jquery.com/Types/#Promise>
  */
 
 /**
+ * Source: <https://api.jquery.com/jQuery.Deferred/>
  * @class jQuery.Deferred
  * @mixins jQuery.Promise
- * @source <http://api.jquery.com/jQuery.Deferred/>
  */
 
 /**
+ * Source: <https://api.jquery.com/Types/#jqXHR>
  * @class jQuery.jqXHR
- * @source <http://api.jquery.com/Types/#jqXHR>
  * @alternateClassName jqXHR
  */
 
 /**
+ * Source: <https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw.Api>
  * @class mw.Api
- * @source <https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw.Api>
  */
 
 /**
+ * Source: <https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw.Message
  * @class mw.Message
- * @source <https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw.Message
  */
 
 /**
+ * Source: <https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw.Title>
  * @class mw.Title
- * @source <https://doc.wikimedia.org/mediawiki-core/master/js/#!/api/mw.Title>
  */
