47c45866decae3a2b44d95e6de34757aab96b258
Matthew Flaschen <mflaschen@wikimedia.org>
2017-12-18 21:37:46 -0500

    Remove Ruby browser tests
    The continuous integration infrastructure for these is being removed
    (or never existed), and our team decided not to invest the time to
    convert them to node.js.
    
    Bug: T171848
    Change-Id: I0faeecb2635f24c40c83aa689b670e69aa381431

--- tests/browser/features/support/pages/article_page.rb
+++
@@ -1,8 +0,0 @@
-class ArticlePage
-  include PageObject
-
-  li(:alerts, css: '#pt-notifications-alert')
-  li(:notices, css: '#pt-notifications-notice')
-  page_section(:alerts, Notifications, css: '#pt-notifications-alert')
-  page_section(:notices, Notifications, css: '#pt-notifications-notice')
-end
--- tests/browser/features/support/pages/special_notifications_page.rb
+++
@@ -1,8 +0,0 @@
-# Special:Notifications page
-class SpecialNotificationsPage
-  include PageObject
-
-  page_url 'Special:Notifications'
-
-  h1(:firstHeading, css: '.firstHeading')
-end
