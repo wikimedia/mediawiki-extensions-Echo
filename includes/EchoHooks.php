8994407a7d870f4a3c8948d3186070c637f4cb5d
Matěj Suchánek <matejsuchanek97@gmail.com>
2018-09-18 14:56:59 +0200

    Remove duplicate notifications after merging users
    Bug: T103747
    Change-Id: Ic2612e5d7861ef7c6a22ab66fd3b9d6ea71f959f

--- includes/EchoHooks.php
+++ includes/EchoHooks.php
@@ -1366,7 +1366,80 @@ class EchoHooks {
 	}
 
 	public static function onMergeAccountFromTo( User &$oldUser, User &$newUser ) {
-		DeferredUpdates::addCallableUpdate( function () use ( $oldUser, $newUser ) {
+		$method = __METHOD__;
+		DeferredUpdates::addCallableUpdate( function () use ( $oldUser, $newUser, $method ) {
+			if ( !$newUser->isAnon() ) {
+				// Select notifications that are now sent to the same user
+				$dbw = MWEchoDbFactory::newFromDefault()->getEchoDb( DB_MASTER );
+				$attributeManager = EchoAttributeManager::newFromGlobalVars();
+				$selfIds = $dbw->selectFieldValues(
+					[ 'echo_notification', 'echo_event' ],
+					'event_id',
+					[
+						'notification_user' => $newUser->getId(),
+						'notification_event = event_id',
+						'notification_user = event_agent_id',
+						'event_type NOT IN (' . $dbr->makeList( $attributeManager->getNotifyAgentEvents() ) . ')'
+					],
+					$method
+				) ?: [];
+
+				// Select newer welcome notification(s)
+				$welcomeIds = $dbw->selectFieldValues(
+					[ 'echo_notification', 'echo_event' ],
+					'event_id',
+					[
+						'notification_user' => $newUser->getId(),
+						'notification_event = event_id',
+						'event_type' => 'welcome',
+					],
+					$method,
+					[
+						'ORDER BY' => 'notification_timestamp ASC',
+						'OFFSET' => 1,
+					]
+				) ?: [];
+
+				// Select newer milestone notifications (per milestone level)
+				$counts = [];
+				$thankYouIds = [];
+				$thankYouRows = $dbw->select(
+					[ 'echo_notification', 'echo_event' ],
+					'echo_event.*',
+					[
+						'notification_user' => $newUser->getId(),
+						'notification_event = event_id',
+						'event_type' => 'thank-you-edit',
+					],
+					$method,
+					[ 'ORDER BY' => 'notification_timestamp ASC' ]
+				) ?: [];
+				foreach ( $thankYouRows as $row ) {
+					$event = EchoEvent::newFromRow( $row );
+					$editCount = $event ? $event->getExtraParam( 'editCount' ) : null;
+					if ( $editCount ) {
+						if ( isset( $counts[$editCount] ) ) {
+							$thankYouIds[] = $row->event_id;
+						} else {
+							$counts[$editCount] = true;
+						}
+					}
+				}
+
+				// Delete notifications
+				$ids = array_merge( $selfIds, $welcomeIds, $thankYouIds );
+				if ( $ids ) {
+					$dbw->delete(
+						'echo_notification',
+						[
+							'notification_user' => $newUser->getId(),
+							'notification_event' => $ids
+						],
+						$method
+					);
+				}
+			}
+
 			MWEchoNotifUser::newFromUser( $oldUser )->resetNotificationCount();
 			if ( !$newUser->isAnon() ) {
 				MWEchoNotifUser::newFromUser( $newUser )->resetNotificationCount();
