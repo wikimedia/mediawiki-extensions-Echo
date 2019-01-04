fdc2b42b2b0e5b7a3d2997c702b294dbe7df8d83
Umherirrender <umherirrender_de.wp@web.de>
2019-01-04 15:03:05 +0100

    Declare class properties in Formatter and WikiRequest
    Change-Id: Iaf4c2f7bb75896d3daafbb445f7b5f6341543f8e

--- includes/formatters/EchoEventDigestFormatter.php
+++ includes/formatters/EchoEventDigestFormatter.php
@@ -7,6 +7,13 @@
  * arguments passed in the constructor (user and language)
  */
 abstract class EchoEventDigestFormatter {
+
+	/** @var User */
+	protected $user;
+
+	/** @var Language */
+	protected $language;
+
 	public function __construct( User $user, Language $language ) {
 		$this->user = $user;
 		$this->language = $language;
