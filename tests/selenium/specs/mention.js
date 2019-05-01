dd4539b522f607c1038dd6322a6c5f28831ffb49
ephemeralwaves <thinkcolorful@thinkcolorful.org>
2019-04-30 23:33:34 -0400

    Selenium: Test that checks for user mention alerts
    Check that a user gets an alert when their username is mentioned by another user.
    
    Bug: T224894
    Change-Id: I303e10a40d1092e4b9f81deef0f1ac2af6f7a435

---
+++ tests/selenium/specs/mention.js
@@ -0,0 +1,27 @@
+var assert = require( 'assert' ),
+	EchoPage = require( '../pageobjects/echo.page' ),
+	UserLoginPage = require( 'wdio-mediawiki/LoginPage' ),
+	Util = require( 'wdio-mediawiki/Util' ),
+	Api = require( 'wdio-mediawiki/Api' );
+
+describe( 'Mention test for Echo', function () {
+	it( 'checks if admin gets alert when mentioned', function () {
+
+		var username = Util.getTestString( 'NewUser-' );
+		var password = Util.getTestString();
+		browser.call( function () {
+			return Api.createAccount( username, password
+			).then( function () {
+				return Api.edit( `User:${username}`, `Hello [[User:${browser.options.username}]] ~~~~`, username, password );
+			} );
+		} );
+		UserLoginPage.login( browser.options.username, browser.options.password );
+
+		EchoPage.alerts.click();
+
+		EchoPage.alertMessage.waitForVisible();
+		let regexp = /‪.*‬ mentioned you on ‪User:.*./;
+		assert( regexp.test( EchoPage.alertMessage.getText() ) );
+	} );
+
+} );
