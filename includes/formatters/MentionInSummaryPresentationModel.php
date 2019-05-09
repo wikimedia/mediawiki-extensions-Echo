05499e044677cbd821b88aa38c3f7cdcafd0a7bb
Roan Kattouw <roan.kattouw@gmail.com>
2019-05-09 14:31:54 -0700

    Fix fatal "Object of class CommentStoreComment could not be converted to string"
    Bug: T221163
    Follows-Up: Icfc85167a636bef95daab236ab80113c1a3cf41b
    Change-Id: I92811ae9e383f431ef939b004c7d10126e051bbf

--- includes/formatters/MentionInSummaryPresentationModel.php
+++ includes/formatters/MentionInSummaryPresentationModel.php
@@ -23,7 +23,7 @@ class EchoMentionInSummaryPresentationModel extends EchoEventPresentationModel {
 	public function getBodyMessage() {
 		$revision = $this->event->getRevision();
 		if ( $revision && $this->userCan( RevisionRecord::DELETED_COMMENT ) ) {
-			$summary = $revision->getComment();
+			$summary = $revision->getComment()->text;
 			$summary = Linker::formatComment( $summary );
 			$summary = Sanitizer::stripAllTags( $summary );
 
