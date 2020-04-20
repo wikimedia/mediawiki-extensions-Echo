1560528dc7c77257f8dc7bff3965f31bf9b3c22c
Kosta Harlan <kharlan@wikimedia.org>
2020-04-20 21:40:07 +0200

    Add page linked event title muted list
    * Add a section on the preference form to allow users to mute articles
      from generating "page linked" notices
    * The preference will save the article title as an article ID
    
    Depends-On: Ia0ddf78646ec4c8ae0b84b6d5b46ef5e51c8e8c1
    Bug: T46787
    Change-Id: I67f751eae5fdc5bccff7fe3047227d432c1cb8d5

--- includes/EchoHooks.php
+++ includes/EchoHooks.php
@@ -2,6 +2,7 @@
 
 use MediaWiki\Logger\LoggerFactory;
 use MediaWiki\MediaWikiServices;
+use MediaWiki\Preferences\MultiTitleFilter;
 use MediaWiki\Preferences\MultiUsernameFilter;
 use MediaWiki\Revision\RevisionRecord;
 
@@ -503,6 +504,12 @@ class EchoHooks {
 				'label-message' => 'echo-pref-notifications-blacklist',
 				'section' => 'echo/blocknotificationslist',
 				'filter' => MultiUsernameFilter::class,
+			];
+			$preferences['echo-notifications-page-linked-title-muted-list'] = [
+				'type' => 'titlesmultiselect',
+				'label-message' => 'echo-pref-notifications-page-linked-title-muted-list',
+				'section' => 'echo/mutedpageslist',
+				'filter' => ( new MultiTitleFilter( new TitleFactory() ) )
 			];
 		}
 	}
