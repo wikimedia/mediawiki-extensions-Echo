e670bb76c9c5881f6d9bcd8bf803cf5fd94212a7
Reedy <reedy@wikimedia.org>
2022-12-20 14:20:59 +0000

    patch-echo_push_subscription-add-column-eps_topic.sql: Add table prefix
    Bug: T322143
    Change-Id: I38f465a3e1ab8059beb94fc85ffe1729379d5b7a
    Follows-Up: I0759b82ad91849880c784e412e04dd53f26df6a2
    (cherry picked from commit 1baec7712a268ece28d0ed4107b5eb5f3c44f4e9)

--- sql/mysql/patch-echo_push_subscription-add-column-eps_topic.sql
+++ sql/mysql/patch-echo_push_subscription-add-column-eps_topic.sql
@@ -1,3 +1,3 @@
-ALTER TABLE echo_push_subscription
+ALTER TABLE /*_*/echo_push_subscription
 ADD COLUMN eps_topic TINYINT UNSIGNED;
 CREATE INDEX /*i*/eps_topic ON /*_*/echo_push_subscription (eps_topic);
