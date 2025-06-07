2741570a9f12377680fe7550f25d3ee9a6dd2a09
Fomafix <fomafix@googlemail.com>
2025-06-07 08:53:41 +0000

    Phan: Remove EventLogging
    The relation to EventLogging was removed in
    f4765cdd25599fdb2b6f35a004b2d0acbdf45fc9.
    
    Change-Id: I972f5fc8f380b6b1433403ebe2f5aa996b7a8703

--- .phan/config.php
+++ .phan/config.php
@@ -5,7 +5,6 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 $cfg['directory_list'] = array_merge(
 	$cfg['directory_list'],
 	[
-		'../../extensions/EventLogging',
 		'../../extensions/CentralAuth',
 		'../../extensions/UserMerge',
 	]
@@ -14,7 +13,6 @@ $cfg['directory_list'] = array_merge(
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
-		'../../extensions/EventLogging',
 		'../../extensions/CentralAuth',
 		'../../extensions/UserMerge',
 	]
