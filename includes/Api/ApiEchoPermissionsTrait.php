14bcb37f8b9032908dd83731fb16739e7c39dba9
Martin Urbanec <martin.urbanec@wikimedia.cz>
2026-03-31 11:18:29 +0200

    refactor: Use a trait to check for reading permissions
    The removal of dieReadOnly() in ApiEchoMarkRead  was done, as this is
    (at least now) done in ApiMain directly, as part of checkExecutePermissions().
    
    Bug: T420154
    Change-Id: I4e8323d6f10c87163a900ca786c03d5621394c69
    (cherry picked from commit 95f6a9bd103f17074d9ec0bb3b519fa3e6332f3d)

---
+++ includes/Api/ApiEchoPermissionsTrait.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace MediaWiki\Extension\Notifications\Api;
+
+use MediaWiki\Api\ApiBase;
+use MediaWiki\Context\ContextSource;
+use MediaWiki\Permissions\Authority;
+use Wikimedia\Message\MessageSpecifier;
+
+trait ApiEchoPermissionsTrait {
+
+	/**
+	 * @see ContextSource::getAuthority()
+	 */
+	abstract public function getAuthority(): Authority;
+
+	/**
+	 * @param string|array|MessageSpecifier $msg See ApiErrorFormatter::addError()
+	 * @param string|null $code See ApiErrorFormatter::addError()
+	 * @param array|null $data See ApiErrorFormatter::addError()
+	 * @param int $httpCode HTTP error code to use
+	 * @see ApiBase::dieWithError()
+	 */
+	abstract public function dieWithError( $msg, $code = null, $data = null, $httpCode = 0 );
+
+	/**
+	 * Ensure the user has read access to their own notifications
+	 *
+	 * Calls dieWithError() with appropriate messaging if omitted.
+	 *
+	 * @return void
+	 */
+	private function checkReadNotificationsPermissions(): void {
+		if ( !$this->getAuthority()->isAllowed( 'echo-read-notifications' ) ) {
+			if ( !$this->getAuthority()->isRegistered() ) {
+				$this->dieWithError( 'apierror-mustbeloggedin-generic', 'login-required' );
+			} else {
+				$this->dieWithError( 'apierror-permissiondenied', 'echo-read-notifications' );
+			}
+		}
+	}
+}
