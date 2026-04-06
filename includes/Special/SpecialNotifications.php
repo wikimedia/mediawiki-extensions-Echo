wikimedia/mediawiki-extensions-Echo

commit fb181ba07e1b735361d8ad40c1e78558017184a3

Use OOJS pending element for JS loader
Fixes dark mode support by not using a light mode -specific embedded
GIF.

Change-Id: I3324c0278a3c236089994e0f0021e4539f8cf28d

includes/Special/SpecialNotifications.php
@@ -53,7 +53,7 @@ class SpecialNotifications extends SpecialPage {
 		$notifications = $pager->getNotifications();
 
 		$noJSDiv = new OOUI\Tag();
-		$noJSDiv->addClasses( [ 'mw-echo-special-nojs' ] );
+		$noJSDiv->addClasses( [ 'mw-echo-special-nojs', 'oo-ui-pendingElement-pending' ] );
 
 		// If there are no notifications, display a message saying so
 		if ( !$notifications ) {

modules/nojs/mw.echo.special.less
@@ -10,8 +10,6 @@
 
 .client-js .mw-echo-special-nojs {
 	min-height: 5em;
-	/* @embed */
-	background-image: url( ../../images/pending.gif );
 
 	.mw-echo-special-container {
 		display: none;

commit fec6bca3125c72f6ffad2486ccab56005345588d

Fix "pending" animation on the special page
Follow-up to fb181ba07e1b735361d8ad40c1e78558017184a3,
which caused the animation to play forever when the page
was loaded without JS. (Reported at https://w.wiki/KhY9)

Introduce separate JS-only and no-JS elements, and tweak
the JS-only loader size to match the real JS interface.

Change-Id: If565e1e8357fa5bea651a467386bb463f084d101

includes/Special/SpecialNotifications.php
@@ -54,8 +54,12 @@ class SpecialNotifications extends SpecialPage {
 		$pager->setLimit( $this->getRequest()->getInt( 'limit', self::DISPLAY_NUM ) );
 		$notifications = $pager->getNotifications();
 
+		$jsDiv = new OOUI\Tag();
+		$jsDiv->addClasses( [ 'mw-echo-special-js', 'oo-ui-pendingElement-pending' ] );
+		$out->addHTML( (string)$jsDiv );
+
 		$noJSDiv = new OOUI\Tag();
-		$noJSDiv->addClasses( [ 'mw-echo-special-nojs', 'oo-ui-pendingElement-pending' ] );
+		$noJSDiv->addClasses( [ 'mw-echo-special-nojs' ] );
 
 		// If there are no notifications, display a message saying so
 		if ( !$notifications ) {

modules/nojs/mw.echo.special.less
@@ -8,12 +8,17 @@
 	max-width: 600px;
 }
 
-.client-js .mw-echo-special-nojs {
+.client-js .mw-echo-special-js {
 	min-height: 5em;
+	max-width: @specialpage-width;
+}
 
-	.mw-echo-special-container {
-		display: none;
-	}
+.client-nojs .mw-echo-special-js {
+	display: none;
+}
+
+.client-js .mw-echo-special-nojs {
+	display: none;
 }
 
 // Hide the preferences link